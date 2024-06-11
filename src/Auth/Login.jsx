143577b093dba01355319a4f8cf5cab547b93b6c
JemishDev07 <jemishdev@gmail.com>
2024-06-11 15:51:10 -0400

    test

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,32 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import {Alert, Spin, Card, Flex, Form, Typography, Input, Button} from 'antd';
-import useLogin from '../Hooks/useLogin';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Alert, Spin, Card, Flex, Form, Typography, Input, Button } from "antd";
+import useLogin from "../Hooks/useLogin";
 
+const Login = () => {
+  const { error, loading, loginUser } = useLogin();
+  const handleLogin = async (values) => {
+    await loginUser(values);
+  };
 
-const Login = () =>{
+  return (
+    <Card className="form-container">
+      <Flex>
+        {/* form */}
 
-    const {error, loading, loginUser} = useLogin();
-    const handleLogin =async (values)=>{
-        await loginUser(values);
-    }
+        <Flex vertical flex={1}>
+          <Typography.Title level={3} strong className="title">
+            Sign In
+          </Typography.Title>
 
-    return <Card className='form-container'>
-        <Flex>
-            {/* form */}
+          <Typography.Text type="secondary" strong className="slogan">
+            Continue to your trip
+          </Typography.Text>
 
-            <Flex vertical flex={1} >
-                <Typography.Title level={3} strong className="title">
-                    Sign In
-                </Typography.Title>
-
-                <Typography.Text type="secondary" strong className='slogan'>
-                    Continue to your trip
-                </Typography.Text>
-
-                <Form layout = "vertical" onFinish={handleLogin} autoComplete='off'>
-
-                    {/* <Form.Item 
+          <Form layout="vertical" onFinish={handleLogin} autoComplete="off">
+            {/* <Form.Item 
                         label='Full Name' 
                         name="name"
                         rules={[
@@ -41,41 +39,37 @@ const Login = () =>{
 
                     </Form.Item> */}
 
-                    <Form.Item 
-                        label='Email' 
-                        name="email"
-                        rules={[
-                            {
-                            required: true,
-                            message: 'please input your Email'
-                            },
-                            {
-                                type: 'email',
-                                message: 'The input is not valid Email'
-                            }
-                        ]}
-                        >
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "please input your Email",
+                },
+                {
+                  type: "email",
+                  message: "The input is not valid Email",
+                },
+              ]}
+            >
+              <Input size="large" placeholder="Enter your full name" />
+            </Form.Item>
 
-                        <Input size="large" placeholder="Enter your full name" />
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "please input your Password",
+                },
+              ]}
+            >
+              <Input.Password size="large" placeholder="Enter your Password" />
+            </Form.Item>
 
-                    </Form.Item>
-
-                    <Form.Item 
-                        label='Password' 
-                        name="password"
-                        rules={[
-                            {
-                            required: true,
-                            message: 'please input your Password'
-                            },
-                        ]}
-                        >
-
-                        <Input.Password size="large" placeholder="Enter your Password" />
-
-                    </Form.Item>
-
-                    {/* <Form.Item 
+            {/* <Form.Item 
                         label='Password' 
                         name="passwordConfirm"
                         rules={[
@@ -90,44 +84,43 @@ const Login = () =>{
 
                     </Form.Item> */}
 
-                    {error && <Alert description={error} type='error' showIcon closable className='alert' />}
+            {error && (
+              <Alert
+                description={error}
+                type="error"
+                showIcon
+                closable
+                className="alert"
+              />
+            )}
 
-                    <Form.Item>
-                        <Button 
-                            type={`${loading ? '': 'primary'}`}
-                            htmlType='submit'
-                            size='large'
-                            className='btn'
-                            >
-                            {loading ? <Spin/> : 'Sign In'}
-                            
-                        </Button>
-                    </Form.Item>
+            <Form.Item>
+              <Button
+                type={`${loading ? "" : "primary"}`}
+                htmlType="submit"
+                size="large"
+                className="btn"
+              >
+                {loading ? <Spin /> : "Sign In"}
+              </Button>
+            </Form.Item>
 
-                    <Form.Item>
-                        <Link to="/">
-                            <Button 
-                            
-                            htmlType='submit'
-                            size='large'
-                            className='btn'
-                            >
-                                Create an account
-                            </Button>
-                        </Link>
-                    </Form.Item>
-
-
-                </Form>
-
-            </Flex>
-
-            {/* Image*/}
-
-            <Flex></Flex>
-
+            <Form.Item>
+              <Link to="/">
+                <Button htmlType="submit" size="large" className="btn">
+                  Create an account
+                </Button>
+              </Link>
+            </Form.Item>
+          </Form>
         </Flex>
+
+        {/* Image*/}
+
+        <Flex></Flex>
+      </Flex>
     </Card>
-}
+  );
+};
 
 export default Login;
